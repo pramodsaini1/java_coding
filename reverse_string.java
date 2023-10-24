4b9e17dac76755d9005f4c29c6b740ce27eccb48
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-23 20:24:24 -0700

    this is reverse the string

--- reverse_string.java
+++ reverse_string.java
@@ -10,16 +10,14 @@ class reverse_string{
             }
             else{
                 for(int j=s1.length()-1;j>=0;j--){
-                    s2=s2+s1.charAt(j);
+                    s2=s2+s1.charAt(i);
                 }
-                 s1="";
+                s1="";
                 s2=s2+"";
             }
-             
         }
         s2=s2+s1;
         System.out.println(s2);
-         
 
     }
 }
