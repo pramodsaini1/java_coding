40be63e3e6091f5bbb8f81c4fd8816c2ee82c560
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-24 20:20:27 -0700

    this is diffrence between abstact and interface class

---
+++ difference.java
@@ -0,0 +1,10 @@
+Abstract                                                                   interface
+create Abstract class prefix the Abstract             interface bydefault Abstract
+keyword
+in abstract class data member bydefault              in interface data member bydefault public static final
+default
+we can define and declare method in                  can't define method in interface
+abstract class
+if we declare method in abstract class,              in interface method bydefault public abstract
+prefix abstract keyword
+have constructor                                     haven't constructor
