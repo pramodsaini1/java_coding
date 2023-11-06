a0d005792c872175a495df891bd4f74f6b19a96b
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-06 15:32:06 -0800

    164. Maximum Gap

---
+++ MaximumGap.java
@@ -0,0 +1,34 @@
+public class MaximumGap {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        int max=Integer.MIN_VALUE;
+        if(nums.length<2){
+            return 0;
+        }
+        for(int i=1;i<nums.length;i++){
+             if(nums[i]-nums[i-1]>max){
+                 max=nums[i]-nums[i-1];
+             }
+        }
+        return max;
+        
+    }
+}
+/*
+ * Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+
+You must write an algorithm that runs in linear time and uses linear extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [3,6,9,1]
+Output: 3
+Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
+Example 2:
+
+Input: nums = [10]
+Output: 0
+Explanation: The array contains less than 2 elements, therefore return 0.
+ */
