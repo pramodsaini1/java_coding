a8c0bcb4ae7ac8e08a245fb116e15cd95668300e
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-06 14:41:58 -0800

    70. Climbing Stairs

---
+++ climbing.java
@@ -0,0 +1,33 @@
+public class climbing {
+    public int climbStairs(int n) {
+        int steps=0;
+        for(int i=1;i<=n;i++){
+            steps=steps+1;
+        }
+        return steps;
+    }
+}
+/*
+ * You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+ */
