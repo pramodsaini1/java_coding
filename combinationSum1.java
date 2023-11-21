8d0c4657cef204d50acd98c799d755a27f284068
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-20 19:06:22 -0800

    40. Combination Sum II

---
+++ combinationSum1.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class combinationSum1 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+         List<List<Integer>>res = new ArrayList<List<Integer>>();
+        List<Integer>list=new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSum3(0,candidates,target,res,list);
+        return res;
+    }
+     private  void combinationSum3(int index,int []arr,int target,List<List<Integer>> res, List<Integer> list){
+         
+            if(target==0){
+                res.add(new ArrayList<>(list));
+                 return ;
+            }
+            
+       
+       for(int i = index; i < arr.length;i++) {
+            if(i > index && arr[i] == arr[i-1]) continue; 
+            if(arr[i]>target) break; 
+            
+            list.add(arr[i]); 
+            combinationSum3(i+1, arr, target - arr[i], res,list); 
+            list.remove(list.size() - 1); 
+        }
+    }
+}
+/*
+ * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+ 
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+ */
