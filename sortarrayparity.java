2e54024eaffa64d42567ce819871c5b0e68cf2e9
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-29 19:33:42 -0800

    922. Sort Array By Parity II

---
+++ sortarrayparity.java
@@ -0,0 +1,54 @@
+public class sortarrayparity {
+    public int[] sortArrayByParityII(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0 && nums[i]%2!=0){
+                for(int j=i+1;j<nums.length;j++){
+                    if(nums[j]%2==0){
+                        int temp=nums[i];
+                        nums[i]=nums[j];
+                        nums[j]=temp;
+                        break;
+                    }
+                }
+            }
+            if(i%2==1 && nums[i]%2!=1){
+                for(int j=i+1;j<nums.length;j++){
+                    if(nums[j]%2==1){
+                       int temp=nums[i];
+                        nums[i]=nums[j];
+                        nums[j]=temp;
+                        break;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+}
+/*
+ * Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+Return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+Example 2:
+
+Input: nums = [2,3]
+Output: [2,3]
+ 
+
+Constraints:
+
+2 <= nums.length <= 2 * 104
+nums.length is even.
+Half of the integers in nums are even.
+0 <= nums[i] <= 1000
+ */
