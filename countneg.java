fffbf435fccfedd6dacb8601b0379fc51d43955b
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-12 19:43:25 -0800

    1351. Count Negative Numbers in a Sorted Matrix

---
+++ countneg.java
@@ -0,0 +1,28 @@
+public class countneg {
+    public int countNegatives(int[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]<0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+/*
+ * Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+ 
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+ */
