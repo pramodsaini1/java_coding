2b751761af247dc283fb5f0a2d7baf589f25a9a1
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-13 17:31:20 -0800

    1539. Kth Missing Positive Number

---
+++ BinarySearch/infinitearraysearch.java
@@ -0,0 +1,19 @@
+package BinarySearch;
+
+public class infinitearraysearch {
+    // public static void main(String[] args) {
+    //     int arr[]={2,3,}
+    // }
+    // static int ans(int arr[],int target){
+    //     // first find the range
+    //     // first start with a box of size 2
+    //     int start=0;
+    //     int end=1;
+
+    //     //condition for the target to lie in the range
+    //     while (target>arr[end]) {
+    //         int newstart=end+1;
+            
+    //     }
+    // }
+}
