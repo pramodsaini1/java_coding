504214aa986eb01a4cb3236245ef45b8cfa9a0bd
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-26 20:47:20 -0800

    this is compare the string

---
+++ String/compare.java
@@ -0,0 +1,9 @@
+package String;
+
+public class compare {
+     public static void main(String[] args) {
+        String s="pramod";
+        String s1="pk";
+        System.out.println(s==s1);
+     }
+}
