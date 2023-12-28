90ce449f42b3632f7deed840fe0eb4bc7835ab05
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-27 20:23:12 -0800

    this is comparision of the string

---
+++ String/com.java
@@ -0,0 +1,12 @@
+package String;
+
+public class com {
+    public static void main(String[] args) {
+        // String a="pramod";
+        // String b="pramod";
+        // System.out.println(a==b);
+        String a=new String("pramod");
+        String b=new String("pramod");
+        System.out.println(a.equals(b));
+    }
+}
