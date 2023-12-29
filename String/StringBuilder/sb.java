6d2c7171255d6054f838a9e79f84a12247019ad1
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-28 20:37:41 -0800

    this is concept of the StringBuilder

---
+++ String/StringBuilder/sb.java
@@ -0,0 +1,13 @@
+package String.StringBuilder;
+
+public class sb {
+    public static void main(String[] args) {
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<26;i++){
+            char ch= (char)('a'+i);
+            sb.append(ch);
+        }
+        System.out.println(sb.toString());
+        
+    }
+}
