b617c4dabfbbe443b5389eec85dc03e57e8131cb
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-28 20:45:01 -0800

    this is methods of the string

---
+++ String/StringBuilder/methods.java
@@ -0,0 +1,13 @@
+package String.StringBuilder;
+
+import java.util.Arrays;
+
+public class methods {
+    public static void main(String[] args) {
+        String name="pramod Saini";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toUpperCase());
+        System.out.println(Arrays.toString(name.split(" ")));
+
+    }
+}
