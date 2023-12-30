81b494ee5eb6448d0e407d4456f5859afa9a7a5a
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-29 18:17:49 -0800

    2928. Distribute Candies Among Children I

---
+++ String/p.java
@@ -0,0 +1,14 @@
+package String;
+
+public class p {
+    public static void main(String[] args) {
+        String s="abcdcba";
+
+    }
+    static boolean isPaindrome(String st){
+        st =st.toLowerCase();
+        for (int i = 0; i <=st.length()/2; i++) {
+            
+        }
+    }
+}
