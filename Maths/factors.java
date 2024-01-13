a5159676198e69728327b3ceaf2375ac97a86fcf
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-12 20:12:27 -0800

    print the all number that will divide the these numbers

---
+++ Maths/factors.java
@@ -0,0 +1,14 @@
+package Maths;
+
+public class factors {
+    public static void main(String[] args) {
+        factors(20);
+    }
+    static void factors(int n){
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                System.out.println(i+" ");
+            }
+        }
+    }
+}
