1738faab769aca3405b017cd146e8b8c8ff15e9d
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-15 19:31:33 -0800

    1037. Valid Boomerang

---
+++ Maths/validBoom.java
@@ -0,0 +1,30 @@
+package Maths;
+
+public class validBoom {
+    public boolean isBoomerang(int[][] points) {
+        int x1=points[0][0];
+        int y1=points[0][1];
+        int x2=points[1][0];
+        int y2=points[1][1];
+        int x3=points[2][0];
+        int y3=points[2][1];
+        int area=x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2);
+        return area!=0;
+    }
+}
+/*
+ * Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+
+A boomerang is a set of three points that are all distinct and not in a straight line.
+
+ 
+
+Example 1:
+
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+Example 2:
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+ */
