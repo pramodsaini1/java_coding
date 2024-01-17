3b5f197f4e5beca4c428fa80da614b0f3082f9ef
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-16 20:08:16 -0800

    this is find the reverse digit using the recursion

--- Recursion/digitReverse.java
+++ Recursion/digitReverse.java
@@ -1,6 +1,18 @@
 package Recursion;
 
 public class digitReverse {
-     
+    static int sum=0;
+    static void reverse(int n){
+        if(n==0){
+            return ;
+        }
+        int rem=n%10;
+        sum=sum*10+rem;
+        reverse(n/10);
+    }
+     public static void main(String[] args) {
+        reverse(1234);
+        System.out.print(sum);
+     }
      
 }
