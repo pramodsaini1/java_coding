d672c770c20f6f325253716342a1721a551078cd
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-19 19:26:43 -0800

    1780. Check if Number is a Sum of Powers of Three

---
+++ Maths/powerOfThree.java
@@ -0,0 +1,34 @@
+package Maths;
+
+public class powerOfThree {
+    public boolean checkPowersOfThree(int n) {
+        while(n>0){
+            if(n%3==2) return false;
+            else n /=3;
+        }
+
+       return true;
+   }
+}
+/*
+ * Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
+
+An integer y is a power of three if there exists an integer x such that y == 3x.
+
+ 
+
+Example 1:
+
+Input: n = 12
+Output: true
+Explanation: 12 = 31 + 32
+Example 2:
+
+Input: n = 91
+Output: true
+Explanation: 91 = 30 + 32 + 34
+Example 3:
+
+Input: n = 21
+Output: false
+ */
