79427c8d477e994e7934bd950698a706d583b6e6
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-28 20:58:21 -0800

    this is print the ascii value of the all subset

---
+++ Recursion/String/subset/subSeqAscii.java
@@ -0,0 +1,24 @@
+package Recursion.String.subset;
+
+import java.util.ArrayList;
+
+public class subSeqAscii {
+    public static void main(String[] args) {
+        System.out.println(subSeqAscii("", "abc"));
+    }
+    public static ArrayList<String> subSeqAscii(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> first=subSeqAscii(p+ch, up.substring(1));
+        ArrayList<String> second=subSeqAscii(p, up.substring(1));
+        ArrayList<String> third=subSeqAscii(p+(ch+0), up.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+        return first;
+    }
+}
