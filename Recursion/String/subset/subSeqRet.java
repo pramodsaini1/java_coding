02ddae5e62b76c09f2aca848057bc30baf6c5814
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-28 20:47:44 -0800

    this is find the all subset from the string using the recursion and return the arrayList

---
+++ Recursion/String/subset/subSeqRet.java
@@ -0,0 +1,21 @@
+package Recursion.String.subset;
+
+import java.util.ArrayList;
+
+public class subSeqRet {
+    public static void main(String[] args) {
+        System.out.println(subSeqRet("", "abc"));
+    }
+    public static ArrayList<String> subSeqRet(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String>left=subSeqRet(p+ch, up.substring(1));
+        ArrayList<String>right=subSeqRet(p, up.substring(1));
+        left.addAll(right);
+        return left;
+    }
+}
