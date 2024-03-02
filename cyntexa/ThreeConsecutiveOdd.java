1f39cd78426dd22cf916a325353dea2c1462b41f
pramodsaini <pramodsaini25062000@gmail.com>
2024-03-01 16:36:55 -0800

    1550. Three Consecutive Odds

---
+++ cyntexa/ThreeConsecutiveOdd.java
@@ -0,0 +1,34 @@
+package cyntexa;
+
+public class ThreeConsecutiveOdd {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                count++;
+            }
+            else{
+                count=0;
+            }
+            if(count==3){
+                return true;
+            }
+        }
+        return false;
+     }
+}
+/*
+ * Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+ */
---
+++ cyntexa/allPrimeNumber.java
@@ -0,0 +1,5 @@
+package cyntexa;
+
+public class allPrimeNumber {
+    
+}
