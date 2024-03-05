8b4e575b51890c00b412a5d755d77400f8cc90d4
pramodsaini <pramodsaini25062000@gmail.com>
2024-03-04 17:59:01 -0800

    961. N-Repeated Element in Size 2N Array

---
+++ cyntexa/nrepeatedelement.java
@@ -0,0 +1,40 @@
+package cyntexa;
+
+import java.util.Arrays;
+
+public class nrepeatedelement {
+    public int repeatedNTimes(int[] nums) {
+         int ans=-0;
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                ans=nums[i];
+            }
+        }
+        return ans;
+    }
+}
+/*
+ * You are given an integer array nums with the following properties:
+
+nums.length == 2 * n.
+nums contains n + 1 unique elements.
+Exactly one element of nums is repeated n times.
+Return the element that is repeated n times.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: nums = [5,1,5,2,5,3,5,4]
+Output: 5
+ 
+ */
