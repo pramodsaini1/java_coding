46f24a8e7e3afdabb427bac8bf9b6ee44b5e9e7f
pramodsaini <pramodsaini25062000@gmail.com>
2024-03-22 18:57:47 -0700

    2185. Counting Words With a Given Prefix

---
+++ cyntexa/countWordPrefix.java
@@ -0,0 +1,33 @@
+package cyntexa;
+
+public class countWordPrefix {
+    public int prefixCount(String[] words, String pref) {
+        int count=0;
+        for(int i=0;i<words.length;i++){
+            if(words[i].startsWith(pref)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+/*
+ * You are given an array of strings words and a string pref.
+
+Return the number of strings in words that contain pref as a prefix.
+
+A prefix of a string s is any leading contiguous substring of s.
+
+ 
+
+Example 1:
+
+Input: words = ["pay","attention","practice","attend"], pref = "at"
+Output: 2
+Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+Example 2:
+
+Input: words = ["leetcode","win","loops","success"], pref = "code"
+Output: 0
+Explanation: There are no strings that contain "code" as a prefix.
+ */
