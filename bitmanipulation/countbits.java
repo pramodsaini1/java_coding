ad65f4ae3ed21e4fc23057a0abb286c782560ae8
pramodsaini <pramodsaini25062000@gmail.com>
2024-03-31 18:41:27 -0700

    338. Counting Bits

---
+++ bitmanipulation/countbits.java
@@ -0,0 +1,38 @@
+package bitmanipulation;
+
+public class countbits {
+    public int[] countBits(int n) {
+        int ans[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            ans[i]=ans[i/2]+i%2;
+
+        }
+        return ans;
+    }
+}
+/*
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+ */
